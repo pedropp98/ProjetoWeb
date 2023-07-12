d651cacbf027716e054cc8eb53d907c1a97234ed
Pedro Perez <pedropc@usp.br>
2023-07-12 01:09:54 -0300

    Joana DArc

--- projeto/backend/Controller/AdminController.js
+++ projeto/backend/Controller/AdminController.js
@@ -15,63 +15,63 @@ exports.get = (req, res) => {
       });
 };
 
-exports.get = (req, res) => {
-   const id = req.params.id;
- 
-   Admin.findById(id)
-     .then((data) => {
-       if (!data) {
-         return res.status(404).json({ error: 'Data not found' });
-       }
- 
-       res.json(data).status(200);
-     })
-     .catch((error) => {
-       console.error('Error:', error);
-       res.status(500).json({ error: 'Internal server error' });
-     });
- };
-
 exports.post = (req, res) => {
    console.log(`Requisicao POST: ${req.body}`);
  
-   console.log(`Nome: ${req.body.nome}`);
+   console.log(`Nome: ${req.body.name}`);
+   console.log(`email: ${req.body.email}`);
+   console.log(`rg: ${req.body.rg}`);
+   console.log(`address: ${req.body.address}`);
+   console.log(`pass: ${req.body.password}`);
    
-   const admin = new Admin({
-     nome: req.body.nome,
-   });
+   const { name, email, rg, address, password } = req.body;
+
+  // Validate the required fields
+  if (!email || !rg || !password) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  // Create a new user
+  const newUser = new Admin({
+    name,
+    email,
+    rg,
+    address,
+    password,
+  });
+
  
-   admin.save()
+  newUser.save()
       .then((response) => {
          console.log(`Resposta: ${response}`);
          res.json(response).status(200);
       })
       .catch((error) => {
-         console.log(`Busca por ${Admin} nao funcionou: ${error}`);
+         console.log(`Cadastro de: ${newUser} nao funcionou: ${error}`);
       });
 };
 
 exports.put = (req, res) => {
-   const id = req.params.id;
-   console.log(`Requisicao GET: ${req.body} ID: ${id}`);
+   const nomeAntigo = req.body.nomeAntigo; // Nome antigo a ser buscado
+   const novoNome = req.body.novoNome; // Novo nome para atualização
  
-   const updateProduct = {
-      title: req.body.title,
-      slug: req.body.slug,
-      description: req.body.description,
-      price: req.body.price,
-      quantity: req.body.quantity
-   };
-
-   Products.findByIdAndUpdate(id, updateProduct, {new : true})
-      .then(updateProduct => {
-         console.log(`Produto atualizado: ${updateProduct}`);
-         res.json(updateProduct).status(200);
-      })
-      .catch(error => {
-         console.log(`Erro atualizando o produto: ${error}`);
-         res.status(500).json({error: 'Erro atualizando o produto'});
-      })
+   Admin.findOneAndUpdate(
+     { nome: nomeAntigo }, // Critério de busca
+     { nome: novoNome }, // Valores atualizados
+   )
+     .then((admAtualizado) => {
+       if (admAtualizado) {
+         console.log('Dado atualizado com sucesso:', admAtualizado);
+         res.json(admAtualizado).status(200);
+       } else {
+         console.log('Dado não encontrado');
+         res.status(404).send('Dado não encontrado');
+       }
+     })
+     .catch((error) => {
+       console.log('Erro ao atualizar o dado:', error);
+       res.status(500).send('Erro ao atualizar o dado');
+     });
  };
 
 exports.delete = (req, res) => {
--- projeto/backend/Models/AdminModel.js
+++ projeto/backend/Models/AdminModel.js
@@ -3,9 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var admin = new Schema({
-   nome:{
+   name:{
       type : String,
       required : false
+   },
+   email : {
+      type : String,
+      required : true
+   },
+   rg : {
+      type : String,
+      required : true
+   },
+   address : {
+      type : String,
+      required : false
+   },
+   password : {
+      type : String,
+      required : true
    }
 });
 
