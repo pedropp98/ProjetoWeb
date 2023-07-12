ab5751e23454f1d7ec2c54d895e5860bc12e0b55
Pedro Perez <pedropc@usp.br>
2023-07-12 05:08:17 -0300

    TAQUI SABOSTA

--- projeto/backend/Routes/AdminRoutes.js
+++ projeto/backend/Routes/AdminRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const controller = require('../Controller/AdminController');
 
 router.get('/admin', controller.get);
+router.get('/admin/:id', controller.getOneById);
 router.post('/admin', controller.post);
 router.put('/admin/:id', controller.put);
 router.delete('/admin', controller.delete);
